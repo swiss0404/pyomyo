c97eb5bc8993ef9c81f8d01cf36fc757a1d1f087
swiss0404 <swiss0404@gmail.com>
2022-03-16 14:56:39 +0700

    clean up comments

--- examples/myo_imu_examp.py
+++ examples/myo_imu_examp.py
@@ -63,38 +63,6 @@ def init():
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
 
 
-def cleanSerialBegin():
-    if(useQuat):
-        try:
-            w = float(line.split('w')[1])
-            nx = float(line.split('a')[1])
-            ny = float(line.split('b')[1])
-            nz = float(line.split('c')[1])
-        except Exception:
-            pass
-    else:
-        try:
-            yaw = float(line.split('y')[1])
-            pitch = float(line.split('p')[1])
-            roll = float(line.split('r')[1])
-        except Exception:
-            pass
-
-
-def read_data():
-    if(useQuat):
-        w = float(line.split('w')[1])
-        nx = float(line.split('a')[1])
-        ny = float(line.split('b')[1])
-        nz = float(line.split('c')[1])
-        return [w, nx, ny, nz]
-    else:
-        yaw = float(line.split('y')[1])
-        pitch = float(line.split('p')[1])
-        roll = float(line.split('r')[1])
-        return [yaw, pitch, roll]
-
-
 def draw(w, nx, ny, nz):
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
@@ -173,8 +141,7 @@ def quat_to_ypr(q):
     roll  *= 180.0 / math.pi
     return [yaw, pitch, roll]
 
-useSerial = True # set true for using serial for data transmission, false for wifi
-useQuat = False   # set true for using quaternions, false for using y,p,r angles
+
 
 if __name__ == "__main__":
     video_flags = OPENGL | DOUBLEBUF
@@ -191,10 +158,6 @@ if __name__ == "__main__":
     max_roll = 15
     min_pitch = -15
     max_pitch = 15
-    # min_roll = 100000
-    # max_roll = -100000
-    # min_pitch = 100000
-    # max_pitch = -100000
     neutral_roll = 0
     neutral_pitch = 0
     neutral_yaw = 0
@@ -207,32 +170,15 @@ if __name__ == "__main__":
                 # print("Quaternions:", quat)
                 # print("Acceleration:", acc)
                 # print("Gyroscope:", gyro)
-                # line = "w"+str(quat[0]/16384)+"w"+"a"+str(quat[1]/16384)+"a"+"b"+str(quat[2]/16384)+"b"+"c"+str(quat[3]/16384)+"c"
                 [w, nx, ny, nz] = [x/16384 for x in quat]
                 [yaw, pitch , roll] = quat_to_ypr([w, nx, ny, nz])
                 draw(1, yaw - neutral_yaw, pitch - neutral_pitch, roll - neutral_roll)
-                # if(useQuat):
-                #     [w, nx, ny, nz] = read_data()
-                # else:
-                #     [yaw, pitch, roll] = read_data()
-                # if(useQuat):
-                #     draw(w, nx, ny, nz)
-                # else:
-                #     draw(1, yaw, pitch, roll)
                 pygame.display.flip()
                 if min_roll < roll and roll < max_roll and min_pitch < pitch and pitch < max_pitch:
                     print(count)
                     count+=1
-                # if roll < min_roll:
-                #     min_roll = roll
-                # if roll > min_roll:
-                #     max_roll = roll
-                # if pitch < min_pitch:
-                #     min_pitch = pitch
-                # if pitch > min_pitch:
-                #     max_pitch = pitch
-                # frames += 1
-                # print("fps: %d" % ((frames*1000)/(pygame.time.get_ticks()-ticks)))
+                frames += 1
+                print("fps: %d" % ((frames*1000)/(pygame.time.get_ticks()-ticks)))
 
                 for ev in pygame.event.get():
                     if ev.type == QUIT or (ev.type == KEYDOWN and ev.unicode == 'q'):
@@ -247,9 +193,7 @@ if __name__ == "__main__":
                             neutral_roll = 0 
                             neutral_pitch = 0
                             neutral_yaw = 0
-                    # elif ev.type == KEYUP:
-                    #     if K_0 <= ev.key <= K_9 or K_KP0 <= ev.key <= K_KP9:
-                    #         hnd.recording = -1
+
     except KeyboardInterrupt:
         print(min_roll,max_roll,min_pitch,max_pitch)
         print("Quitting")
