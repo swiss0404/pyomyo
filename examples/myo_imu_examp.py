c696627c44a15223fc8ae435aa6751fdd6960fb4
swiss0404 <swiss0404@gmail.com>
2022-03-16 13:07:51 +0700

    add visulaization and calibration

--- examples/myo_imu_examp.py
+++ examples/myo_imu_examp.py
@@ -1,10 +1,17 @@
 import multiprocessing
+
+from pyparsing import line
 from pyomyo import Myo, emg_mode
 import os
+import pygame
+import math
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from pygame.locals import *
 
 def cls():
 	# Clear the screen in a cross platform way
-	# https://stackoverflow.com/questions/517970/how-to-clear-the-interpreter-console
+	# https://stackoverflow.com/questicoons/517970/how-to-clear-the-interpreter-console
     os.system('cls' if os.name=='nt' else 'clear')
 
 # ------------ Myo Setup ---------------
@@ -31,20 +38,219 @@ def worker(q):
 	print("Worker Stopped")
 
 # -------- Main Program Loop -----------
+
+
+def resizewin(width, height):
+    """
+    For resizing window
+    """
+    if height == 0:
+        height = 1
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, 1.0*width/height, 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def init():
+    glShadeModel(GL_SMOOTH)
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LEQUAL)
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+
+
+def cleanSerialBegin():
+    if(useQuat):
+        try:
+            w = float(line.split('w')[1])
+            nx = float(line.split('a')[1])
+            ny = float(line.split('b')[1])
+            nz = float(line.split('c')[1])
+        except Exception:
+            pass
+    else:
+        try:
+            yaw = float(line.split('y')[1])
+            pitch = float(line.split('p')[1])
+            roll = float(line.split('r')[1])
+        except Exception:
+            pass
+
+
+def read_data():
+    if(useQuat):
+        w = float(line.split('w')[1])
+        nx = float(line.split('a')[1])
+        ny = float(line.split('b')[1])
+        nz = float(line.split('c')[1])
+        return [w, nx, ny, nz]
+    else:
+        yaw = float(line.split('y')[1])
+        pitch = float(line.split('p')[1])
+        roll = float(line.split('r')[1])
+        return [yaw, pitch, roll]
+
+
+def draw(w, nx, ny, nz):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    glTranslatef(0, 0.0, -7.0)
+
+    drawText((-2.6, 1.8, 2), "PyTeapot", 18)
+    drawText((-2.6, 1.6, 2), "Module to visualize quaternion or Euler angles data", 16)
+    drawText((-2.6, -2, 2), "Press Escape to exit.", 16)
+
+    if(useQuat):
+        [yaw, pitch , roll] = quat_to_ypr([w, nx, ny, nz])
+        drawText((-2.6, -1.8, 2), "Yaw: %f, Pitch: %f, Roll: %f" %(yaw, pitch, roll), 16)
+        glRotatef(2 * math.acos(w) * 180.00/math.pi, -1 * nx, nz, ny)
+    else:
+        yaw = nx
+        pitch = ny
+        roll = nz
+        drawText((-2.6, -1.8, 2), "Yaw: %f, Pitch: %f, Roll: %f" %(yaw, pitch, roll), 16)
+        glRotatef(-roll, 0.00, 0.00, 1.00)
+        glRotatef(pitch, 1.00, 0.00, 0.00)
+        glRotatef(yaw, 0.00, 1.00, 0.00)
+
+    glBegin(GL_QUADS)
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(1.0, 0.2, -1.0)
+    glVertex3f(-1.0, 0.2, -1.0)
+    glVertex3f(-1.0, 0.2, 1.0)
+    glVertex3f(1.0, 0.2, 1.0)
+
+    glColor3f(1.0, 0.5, 0.0)
+    glVertex3f(1.0, -0.2, 1.0)
+    glVertex3f(-1.0, -0.2, 1.0)
+    glVertex3f(-1.0, -0.2, -1.0)
+    glVertex3f(1.0, -0.2, -1.0)
+
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(1.0, 0.2, 1.0)
+    glVertex3f(-1.0, 0.2, 1.0)
+    glVertex3f(-1.0, -0.2, 1.0)
+    glVertex3f(1.0, -0.2, 1.0)
+
+    glColor3f(1.0, 1.0, 0.0)
+    glVertex3f(1.0, -0.2, -1.0)
+    glVertex3f(-1.0, -0.2, -1.0)
+    glVertex3f(-1.0, 0.2, -1.0)
+    glVertex3f(1.0, 0.2, -1.0)
+
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(-1.0, 0.2, 1.0)
+    glVertex3f(-1.0, 0.2, -1.0)
+    glVertex3f(-1.0, -0.2, -1.0)
+    glVertex3f(-1.0, -0.2, 1.0)
+
+    glColor3f(1.0, 0.0, 1.0)
+    glVertex3f(1.0, 0.2, -1.0)
+    glVertex3f(1.0, 0.2, 1.0)
+    glVertex3f(1.0, -0.2, 1.0)
+    glVertex3f(1.0, -0.2, -1.0)
+    glEnd()
+
+
+def drawText(position, textString, size):
+    font = pygame.font.SysFont("Courier", size, True)
+    textSurface = font.render(textString, True, (255, 255, 255, 255), (0, 0, 0, 255))
+    textData = pygame.image.tostring(textSurface, "RGBA", True)
+    glRasterPos3d(*position)
+    glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
+
+def quat_to_ypr(q):
+    yaw   = math.atan2(2.0 * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3])
+    pitch = -math.asin(2.0 * (q[1] * q[3] - q[0] * q[2]))
+    roll  = math.atan2(2.0 * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3])
+    pitch *= 180.0 / math.pi
+    yaw   *= 180.0 / math.pi
+    yaw   -= -0.13  # Declination at Chandrapur, Maharashtra is - 0 degress 13 min
+    roll  *= 180.0 / math.pi
+    return [yaw, pitch, roll]
+
+useSerial = True # set true for using serial for data transmission, false for wifi
+useQuat = False   # set true for using quaternions, false for using y,p,r angles
+
 if __name__ == "__main__":
-	p = multiprocessing.Process(target=worker, args=(q,))
-	p.start()
+    video_flags = OPENGL | DOUBLEBUF
+    pygame.init()
+    p = multiprocessing.Process(target=worker, args=(q,))
+    p.start()
+    screen = pygame.display.set_mode((640, 480), video_flags)
+    pygame.display.set_caption("PyTeapot IMU orientation visualization")
+    resizewin(640, 480)
+    init()
+    frames = 0
+    ticks = pygame.time.get_ticks()
+    min_roll = -15
+    max_roll = 15
+    min_pitch = -15
+    max_pitch = 15
+    # min_roll = 100000
+    # max_roll = -100000
+    # min_pitch = 100000
+    # max_pitch = -100000
+    neutral_roll = 0
+    neutral_pitch = 0
+    neutral_yaw = 0
+    count = 0
+    try:
+        while True: # video_flags = OPENGL | DOUBLEBUF
+            while not(q.empty()):
+                imu = list(q.get())
+                quat, acc, gyro = imu
+                # print("Quaternions:", quat)
+                # print("Acceleration:", acc)
+                # print("Gyroscope:", gyro)
+                # line = "w"+str(quat[0]/16384)+"w"+"a"+str(quat[1]/16384)+"a"+"b"+str(quat[2]/16384)+"b"+"c"+str(quat[3]/16384)+"c"
+                [w, nx, ny, nz] = [x/16384 for x in quat]
+                [yaw, pitch , roll] = quat_to_ypr([w, nx, ny, nz])
+                draw(1, yaw - neutral_yaw, pitch - neutral_pitch, roll - neutral_roll)
+                # if(useQuat):
+                #     [w, nx, ny, nz] = read_data()
+                # else:
+                #     [yaw, pitch, roll] = read_data()
+                # if(useQuat):
+                #     draw(w, nx, ny, nz)
+                # else:
+                #     draw(1, yaw, pitch, roll)
+                pygame.display.flip()
+                if min_roll < roll and roll < max_roll and min_pitch < pitch and pitch < max_pitch:
+                    print(count)
+                    count+=1
+                # if roll < min_roll:
+                #     min_roll = roll
+                # if roll > min_roll:
+                #     max_roll = roll
+                # if pitch < min_pitch:
+                #     min_pitch = pitch
+                # if pitch > min_pitch:
+                #     max_pitch = pitch
+                # frames += 1
+                # print("fps: %d" % ((frames*1000)/(pygame.time.get_ticks()-ticks)))
 
-	try:
-		while True:
-			while not(q.empty()):
-				imu = list(q.get())
-				quat, acc, gyro = imu
-				print("Quaternions:", quat)
-				print("Acceleration:", acc)
-				print("Gyroscope:", gyro)
-				cls()
-
-	except KeyboardInterrupt:
-		print("Quitting")
-		quit()
+                for ev in pygame.event.get():
+                    if ev.type == QUIT or (ev.type == KEYDOWN and ev.unicode == 'q'):
+                        raise KeyboardInterrupt()
+                    elif ev.type == KEYDOWN:
+                        if ev.unicode == 'c':
+                            neutral_roll = roll 
+                            neutral_pitch = pitch
+                            neutral_yaw = yaw
+                        elif ev.unicode == 'e':
+                            print("Pressed e, erasing calibration")
+                            neutral_roll = 0 
+                            neutral_pitch = 0
+                            neutral_yaw = 0
+                    # elif ev.type == KEYUP:
+                    #     if K_0 <= ev.key <= K_9 or K_KP0 <= ev.key <= K_KP9:
+                    #         hnd.recording = -1
+    except KeyboardInterrupt:
+        print(min_roll,max_roll,min_pitch,max_pitch)
+        print("Quitting")
+        quit()
