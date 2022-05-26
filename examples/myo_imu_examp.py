7b2f1103c23afffbedcde4721f3258515c11d04b
swiss0404 <swiss0404@gmail.com>
2022-05-26 15:07:30 +0700

    add buffer moe and change global var

--- examples/myo_imu_examp.py
+++ examples/myo_imu_examp.py
@@ -1,5 +1,5 @@
 import multiprocessing
-
+import pandas as pd
 from pyparsing import line
 from pyomyo import Myo, emg_mode
 import os
@@ -8,6 +8,20 @@ import math
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from pygame.locals import *
+
+min_roll = -30
+max_roll = 30
+min_pitch = -30
+max_pitch = 30
+neutral_roll = 0
+neutral_pitch = 0
+neutral_yaw = 0 
+count = 0
+is_recording = False
+is_start = False 
+count_list = [0]*10
+R_myo_tty = str(b'\x00\x03\x00\x00\x08Swiss.np')
+database_file = 'database.csv'
 
 def cls():
 	# Clear the screen in a cross platform way
@@ -18,23 +32,23 @@ def cls():
 q = multiprocessing.Queue()
 
 def worker(q):
-	m = Myo(mode=emg_mode.FILTERED)
-	m.connect()
+	myo_R = Myo(mode=emg_mode.FILTERED)
+	myo_R.connect()
 	
 	def add_to_queue(quat, acc, gyro):
 		imu_data = [quat, acc, gyro]
 		q.put(imu_data)
 
-	m.add_imu_handler(add_to_queue)
+	myo_R.add_imu_handler(add_to_queue)
 	
 	# Orange logo and bar LEDs
-	m.set_leds([128, 128, 0], [128, 128, 0])
+	myo_R.set_leds([128, 128, 0], [128, 128, 0])
 	# Vibrate to know we connected okay
-	m.vibrate(1)
+	myo_R.vibrate(1)
 	
 	"""worker function"""
 	while True:
-		m.run()
+		myo_R.run()
 	print("Worker Stopped")
 
 # -------- Main Program Loop -----------
@@ -68,9 +82,15 @@ def draw(w, nx, ny, nz):
     glLoadIdentity()
     glTranslatef(0, 0.0, -7.0)
 
-    drawText((-2.6, 1.8, 2), "PyTeapot", 18)
-    drawText((-2.6, 1.6, 2), "Module to visualize quaternion or Euler angles data", 16)
-    drawText((-2.6, -2, 2), "Press Escape to exit.", 16)
+    drawText((-2.6, 1.8, 2), "IMU recording", 18)
+    if is_start:
+        if is_recording:
+            drawText((-2.6, 1.6, 2), "RECORDING", 18)
+        else:
+            drawText((-2.6, 1.6, 2), "REST", 18)
+    else:
+        drawText((-2.6, 1.6, 2), "PAUSE", 18)
+    drawText((-2.6, -2, 2), "'s' to start, 'p' to pause, 'c' to calibrate ", 16)
 
     yaw = nx
     pitch = ny
@@ -128,7 +148,7 @@ def drawText(position, textString, size):
 
 def quat_to_ypr(q):
     yaw   = math.atan2(2.0 * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3])
-    pitch = -math.asin(2.0 * (q[1] * q[3] - q[0] * q[2]))
+    pitch = math.asin(-2.0 * (q[1] * q[3] - q[0] * q[2]))
     roll  = math.atan2(2.0 * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3])
     pitch *= 180.0 / math.pi
     yaw   *= 180.0 / math.pi
@@ -136,29 +156,119 @@ def quat_to_ypr(q):
     roll  *= 180.0 / math.pi
     return [yaw, pitch, roll]
 
+def keep_domain(angle):
+    if angle >= 180:
+        angle-=360
+    elif angle <= -180:
+        angle+=360
+    return angle
+
+def check_is_recording_moe(adjusted_roll,adjusted_pitch):
+    global count_list
+    if min_roll < adjusted_roll and adjusted_roll < max_roll and min_pitch < adjusted_pitch and adjusted_pitch < max_pitch:
+        count_list = count_list[1:]
+        count_list.append(1)
+        print(count_list)
+    else: 
+        count_list = count_list[1:]
+        count_list.append(-1)
+        print(count_list)
+    if sum(count_list) >= 0:
+        return False
+    else:
+        return True 
+
+def name_prompt(data):
+    while True:
+        print('please choose your name')
+        for i in range(len(data['name'].unique())):
+            print(str(i) +': '+ data['name'].unique()[i])
+        name_num = input('type in your name number : ')
+        print('\n---------------------------------\n')
+        try:
+            name_num = int(name_num)
+            if 0 <= name_num <= len(data['name'].unique()) - 1:
+                return data['name'].unique()[name_num]
+            else:
+                print('Number is not within the range')
+
+        except ValueError:
+            print('Invalid value')
+
+
+def gesture_prompt(data, name):
+    while True:
+        print('please choose gesture')
+        for i in range(len(data[data.name == name]['gesture'])):
+            print(str(i) +': '+ data[data.name == name]['gesture'].iloc[i])
+        gesture_num = input('type in gesture number: ')
+        print('\n---------------------------------\n')
+        try:
+            gesture_num = int(gesture_num)
+            if 0 <= int(gesture_num) <= len(data[data.name == name]['gesture']) - 1:
+                return data[data.name == name]['gesture'].iloc[gesture_num]
+            else:
+                print('Number is not within the range')
+
+        except ValueError:
+            print('Invalid value')
+            
+
+def mode_prompt ():
+    while True:
+        print('select mode')
+        print('0: recording continuous\n1: record only once\n2: add gesture name')
+        mode = input('type in mode number: ')
+        print('\n---------------------------------\n')
+        try:
+            mode = int(mode)
+            if 0 <= int(mode) <= 2:
+                return mode
+            else:
+                print('Number is not within the range')
+                
+        except ValueError:
+            print('Invalid value')
+
+    
+def add_new_gesture(data,database_file,name):
+    new_gesture_name = input('type new gesture name: ')
+    try:
+        str(new_gesture_name)
+        if new_gesture_name not in data[data.name == name]['gesture'].to_list():
+            data = data.append({'name': name, 'gesture': new_gesture_name ,'repetition' : 0}, ignore_index=True)
+            data.to_csv(open(database_file, "wb" ), index = False)
+        else: 
+            print('This gesture already exists')
+            add_new_gesture(data,database_file,name)
+    except ValueError:
+        print('input invalid') 
+        add_new_gesture(data,database_file,name)
+
 
 
 if __name__ == "__main__":
+    data = pd.read_csv(open(database_file, "rb" ))
+    name = name_prompt(data)
+    mode = mode_prompt()
+    if mode == 2:
+        add_new_gesture(data,database_file,name)
+        data = pd.read_csv(open(database_file, "rb" ))
+    gesture = gesture_prompt(data, name)
+
     video_flags = OPENGL | DOUBLEBUF
     pygame.init()
     p = multiprocessing.Process(target=worker, args=(q,))
     p.start()
     screen = pygame.display.set_mode((640, 480), video_flags)
-    pygame.display.set_caption("PyTeapot IMU orientation visualization")
+    pygame.display.set_caption("IMU orientation visualization")
     resizewin(640, 480)
     init()
     frames = 0
     ticks = pygame.time.get_ticks()
-    min_roll = -15
-    max_roll = 15
-    min_pitch = -15
-    max_pitch = 15
-    neutral_roll = 0
-    neutral_pitch = 0
-    neutral_yaw = 0
-    count = 0
+    print(count_list)
     try:
-        while True: # video_flags = OPENGL | DOUBLEBUF
+        while True:
             while not(q.empty()):
                 imu = list(q.get())
                 quat, acc, gyro = imu
@@ -166,14 +276,21 @@ if __name__ == "__main__":
                 # print("Acceleration:", acc)
                 # print("Gyroscope:", gyro)
                 [w, nx, ny, nz] = [x/16384 for x in quat]
-                [yaw, pitch , roll] = quat_to_ypr([w, nx, ny, nz])
-                draw(1, yaw - neutral_yaw, pitch - neutral_pitch, roll - neutral_roll)
+                try:
+                    [yaw, pitch , roll] = quat_to_ypr([w, nx, ny, nz])
+                    adjusted_yaw = keep_domain(yaw - neutral_yaw)
+                    adjusted_pitch = keep_domain(pitch - neutral_pitch)
+                    adjusted_roll = keep_domain(roll - neutral_roll)
+                except ValueError:
+                    adjusted_yaw = 0
+                    adjusted_pitch = 0
+                    adjusted_roll = 0
+                draw(1, adjusted_yaw, adjusted_pitch, adjusted_roll)
                 pygame.display.flip()
-                if min_roll < roll and roll < max_roll and min_pitch < pitch and pitch < max_pitch:
-                    print(count)
-                    count+=1
-                frames += 1
-                print("fps: %d" % ((frames*1000)/(pygame.time.get_ticks()-ticks)))
+                if is_start:
+                    is_recording = check_is_recording_moe(adjusted_roll,adjusted_pitch)
+                # frames += 1
+                # print("fps: %d" % ((frames*1000)/(pygame.time.get_ticks()-ticks)))
 
                 for ev in pygame.event.get():
                     if ev.type == QUIT or (ev.type == KEYDOWN and ev.unicode == 'q'):
@@ -188,8 +305,14 @@ if __name__ == "__main__":
                             neutral_roll = 0 
                             neutral_pitch = 0
                             neutral_yaw = 0
-
+                        elif ev.unicode == 's':
+                            print("Pressed s, Started")
+                            print('start record')
+                            is_recording = True
+                            is_start = True
+                        elif ev.unicode == 'p':
+                            print("Pressed p, Paused")
+                            is_start = False
     except KeyboardInterrupt:
         print(min_roll,max_roll,min_pitch,max_pitch)
         print("Quitting")
-        quit()
